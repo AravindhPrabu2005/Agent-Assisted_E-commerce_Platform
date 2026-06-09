5824e676705fecf9f6175a537352f9b080753f32
Aravindh Prabu <aravindhprabu2005@gmail.com>
2026-06-09 16:26:11 +0530

    backendURL change for vercel deployment

--- client/src/axiosInstance.js
+++ client/src/axiosInstance.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-     baseURL:"http://localhost:5000"
+     baseURL:"https://ekadai.aravindhprabu.me/"
 });
+// https://ekadai.aravindhprabu.me/
+// http://localhost:5000
 
 export default axiosInstance;
